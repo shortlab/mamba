c0ab8ad5c0c90523abf6e779f61214b61f7b5a82
Michael Short <hereiam@mit.edu>
2014-04-14 13:24:18 -0400

    Separated MAMBA from HOGNOSE

--- src/base/MambaApp.C
+++ src/base/MambaApp.C
@@ -1,7 +1,51 @@
+// Moose Includes
 #include "MambaApp.h"
 #include "Moose.h"
+#include "MambaApp.h"
+#include "Factory.h"
 #include "AppFactory.h"
-#include "ModulesApp.h"
+#include "ActionFactory.h"
+
+// First come the kernel header files
+
+#include "ThermalDiffusion.h"
+#include "AdvectionForHeat.h"
+#include "PressureDarcy.h"
+#include "AdvectionForConcentration.h"
+#include "DiffusionForConcentration.h"
+#include "BoricAcidSinks.h"
+//#include "WaterSaturationTemperature.h"
+
+// Next come the auxilliary kernels
+
+#include "TortuosityAux.h"
+#include "PorosityAux.h"
+#include "WaterSaturationTemperatureAux.h"
+#include "SuperheatTempAux.h"
+#include "PecletAux.h"
+#include "Precipitation_HBO2Aux.h"
+#include "BO3_SolubilityAux.h"
+
+// Next come the materials
+
+#include "CRUDMaterial.h"
+
+// Next come the boundary conditions
+
+#include "CRUDCladNeumannBC.h"
+#include "CRUDCladNeumannBCMultiApp.h"
+#include "CRUDCoolantNeumannBC.h"
+#include "CoupledTsatDirichletBC.h"
+#include "ChimneyEvaporationNeumannBC.h"
+#include "CRUDChimneyConcentrationMixedBC.h"
+
+//userobjects
+#include "WaterSteamEOS.h"
+
+// Finally, the postprocessors
+
+#include "NodalMaxValueFileIO.h"
+#include "AreaAverageBoron.h"
 
 template<>
 InputParameters validParams<MambaApp>()
@@ -13,19 +57,12 @@ InputParameters validParams<MambaApp>()
 MambaApp::MambaApp(const std::string & name, InputParameters parameters) :
     MooseApp(name, parameters)
 {
-  srand(libMesh::processor_id());
-  
+  // Register all the user-created objects for incorporation and use
   Moose::registerObjects(_factory);
-  ModulesApp::registerObjects(_factory);
   MambaApp::registerObjects(_factory);
 
+  // Associate Parser Syntax
   Moose::associateSyntax(_syntax, _action_factory);
-  ModulesApp::associateSyntax(_syntax, _action_factory);
-  MambaApp::associateSyntax(_syntax, _action_factory);
-}
-
-MambaApp::~MambaApp()
-{
 }
 
 void
@@ -37,9 +74,54 @@ MambaApp::registerApps()
 void
 MambaApp::registerObjects(Factory & factory)
 {
-}
+  // First come the kernels
 
-void
-MambaApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
-{
+  // Our new advection-diffusion kernel that accepts an auxKernel's aqueous species diffusivity,
+  // the permeability material property, and the coupled pressure variable (of which the grad is taken)
+
+  registerKernel(AdvectionForConcentration);
+  registerKernel(DiffusionForConcentration);
+  registerKernel(BoricAcidSinks);
+//  registerAux(WaterSaturationTemperature);
+
+  // Our new Darcy flow kernel that accepts the permeability material property
+
+  registerKernel(PressureDarcy);
+
+  // Our new thermal diffusion kernel that accepts the k_cond (thermal conductivity) material property
+
+  registerKernel(ThermalDiffusion);
+  registerKernel(AdvectionForHeat);
+
+  // Next come the auxilliary kernels
+
+  registerAux(TortuosityAux);
+  registerAux(PorosityAux);
+  registerAux(WaterSaturationTemperatureAux);
+  registerAux(SuperheatTempAux);
+  registerAux(PecletAux);
+  registerAux(Precipitation_HBO2Aux);
+  registerAux(BO3_SolubilityAux);
+
+  // Next come the materials
+
+  // Register our new CRUD material class so we can use it.
+
+  registerMaterial(CRUDMaterial);
+
+  // Next come the boundary conditions
+
+  registerBoundaryCondition(CRUDCladNeumannBC);
+  registerBoundaryCondition(CRUDCladNeumannBCMultiApp);
+  registerBoundaryCondition(CRUDCoolantNeumannBC);
+  registerBoundaryCondition(CoupledTsatDirichletBC);
+  registerBoundaryCondition(ChimneyEvaporationNeumannBC);
+  registerBoundaryCondition(CRUDChimneyConcentrationMixedBC);
+
+  //userobjects
+  registerUserObject(WaterSteamEOS);
+
+  // Finally come the postprocessors
+  registerPostprocessor(NodalMaxValueFileIO);
+  registerPostprocessor(AreaAverageBoron);
 }
--- src/main.C
+++ src/main.C
@@ -1,4 +1,19 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
 #include "MambaApp.h"
+//Moose Includes
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
@@ -7,7 +22,7 @@
 // Create a performance log
 PerfLog Moose::perf_log("Mamba");
 
-// Begin the main program.
+ // Begin the main program.
 int main(int argc, char *argv[])
 {
   // Initialize MPI, solvers and MOOSE
